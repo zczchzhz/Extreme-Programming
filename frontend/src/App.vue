df70a01b1a35fafa78d2dc6284927a844b7a9460
Leo666happy <477731294@qq.com>
2025-12-12 11:14:56 +0800

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header style="background-color: #409EFF; color: white; display: flex; align-items: center;">
+        <h2>通讯录管理系统 - 832302220/832302126</h2>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0;
+  padding: 0;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+</style>

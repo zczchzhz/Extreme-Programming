152cfa0ca282ab77f238881c841b30ff11d1d7c8
zczchzhz <2074056583@qq.com>
2025-12-12 14:25:41 +0800

    Add BusinessException class for handling business errors

---
+++ backend/src/main/java/com/contacts/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.contacts.exception;
+
+/**
+ * 业务异常基类
+ */
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
